3bb7fe28636d6d5c9482077e3bde4cd0d63089ce
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-12-15 08:23:01 -0500

    Update Notas

--- notas.py
+++ notas.py
@@ -1,8 +1,11 @@
+"""
+PROGRAMA PARA CALCULAR LAS NOTAS DE N ESTUDIANTES..
+"""
 import os
 alumnos =[]
 isActive = True
-menu = "1. Registrar Alumno\n2. Registrar Nota\n3. Salir\n:)"
-subMenuNotas = ["1. Parciales","2. Quices","3. Trabajos","4. Regresar al menu principal"]
+menu = "1. Registrar Alumno\n2. Registrar Notas\n3. Salir\n:)"
+subMenuNotas = ["Parciales","Quices","Trabajos","Regresar al menu principal"]
 opMenu=0
 while (isActive) :
     os.system("cls")
@@ -15,7 +18,28 @@ while (isActive) :
         if (opMenu == 1):
             pass
         elif (opMenu == 2):
-            pass
+            opNotas = 0
+            isActiveGrades = True
+            while (isActiveGrades):
+                os.system("cls")
+                for i,item in enumerate(subMenuNotas):
+                    print(f"{i+1}. {item}")
+                try:
+                    opNotas = int(input(":)"))
+                except ValueError:
+                    print("Error en el dato de de ingreso")
+                    os.system("pause")
+                else:
+                    if (opNotas == 1):
+                        pass
+                    elif (opNotas == 2):
+                        pass
+                    elif (opNotas == 3):
+                        pass
+                    elif (opNotas == 4):
+                        isActiveGrades = False
+                    else:
+                        pass
         elif (opMenu == 3):
             os.system("cls")
             print("Gracias por usar nuestro sistema")
